62bd6051f038941b286b922250ce035160dff7bb
Alexandre Ribault (Capgemini) <alexandre.ribault@capgemini.com>
2023-05-10 17:27:53 +0200

    Rework of the product data and import all data in the database

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -14,7 +14,6 @@ const productSchema = mongoose.Schema(
         user: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
         name: { type: String, required: true },
         image: { type: String, required: true },
-        brand: { type: String, required: true },
         category: { type: String, required: true },
         description: { type: String, required: true },
         reviews: [reviewSchema],
