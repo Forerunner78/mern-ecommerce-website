c0894bbd98bfeb46b113f15604e1d3f92de4e578
Alexandre Ribault (Capgemini) <alexandre.ribault@capgemini.com>
2023-05-12 18:16:56 +0200

    Integration of redux

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -2,6 +2,8 @@ import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import { Roboto } from "next/font/google";
 import { Container } from "react-bootstrap";
+import { Provider } from "react-redux";
+import store from "@/GlobalRedux/store";
 import "@/styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Head from "next/head";
@@ -21,15 +23,17 @@ export default function App({ Component, pageProps }) {
                 />
             </Head>
             <main className={roboto.className}>
-                <Header />
+                <Provider store={store}>
+                    <Header />
 
-                <div className="layout">
-                    <Container fluid>
-                        <Component {...pageProps} />
-                    </Container>
-                </div>
+                    <div className="layout">
+                        <Container fluid>
+                            <Component {...pageProps} />
+                        </Container>
+                    </div>
 
-                <Footer />
+                    <Footer />
+                </Provider>
             </main>
         </>
     );
