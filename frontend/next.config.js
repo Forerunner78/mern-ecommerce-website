dbe9af1dd95ce0e11adad778dd6cf4faf1557291
Alexandre Ribault (Capgemini) <alexandre.ribault@capgemini.com>
2023-05-22 15:34:23 +0200

    Fetch products informations into the homepage and add images in product cards

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    async rewrites() {
+        return [
+            {
+                source: "/api/products",
+                destination: "http://localhost:5000/api/products",
+            },
+        ];
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,18 +1,36 @@
 import { Col, Row } from "react-bootstrap";
 import products from "@/products";
 import ProductCard from "@/components/ProductCard";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { listProducts } from "@/GlobalRedux/actions/productActions";
 
 const HomeScreen = () => {
+    const dispatch = useDispatch();
+
+    const productList = useSelector((state) => state.productList);
+    const { loading, error, products } = productList;
+
+    useEffect(() => {
+        dispatch(listProducts());
+    }, [dispatch]);
+
     return (
         <>
             <h1>Latest Products</h1>
-            <Row>
-                {products.map((product) => (
-                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                        <ProductCard product={product} />
-                    </Col>
-                ))}
-            </Row>
+            {loading ? (
+                <h2>Loading...</h2>
+            ) : error ? (
+                <h3>{error}</h3>
+            ) : (
+                <Row>
+                    {products.map((product) => (
+                        <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                            <ProductCard product={product} />
+                        </Col>
+                    ))}
+                </Row>
+            )}
         </>
     );
 };
